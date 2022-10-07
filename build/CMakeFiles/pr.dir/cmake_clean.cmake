e78e7f3a5305fa2caa9cba16cc5abf03fb08ddfe
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-10-06 19:02:38 -0500

    Creacion del Proyecto
    Se genera el Cmakelist y se crea el proyecto con solo el codigo para la lectura de la imagen

---
+++ build/CMakeFiles/pr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pr.dir/proyecto.cxx.o"
+  "CMakeFiles/pr.dir/proyecto.cxx.o.d"
+  "pr"
+  "pr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
