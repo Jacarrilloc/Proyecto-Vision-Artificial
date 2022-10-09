55fdcff0d10b7e3b0caf2b00288a051f3e0cf8cf
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-10-09 11:09:30 -0500

    Implementacion de reduccion de ruido
    se implementa la reduccion de ruido en el proyecto

--- proyecto.cxx
+++ proyecto.cxx
@@ -10,6 +10,46 @@ using namespace cv;
 
 list<Mat> imagenes;
 
+void guardarIMagen(Mat imagen,String nombre)
+{
+  imwrite( nombre + ".png", imagen );
+  cout<<"-----IMAGEN " << nombre << ".png CREADA----"<<endl; 
+}
+
+Mat eliminarRuido(Mat imagen)
+{
+  Mat sinRuido;
+  Mat kernel, kernel_2;
+  Point anchor,anchor_2;
+  double delta;
+  int ddepth;
+  int kernel_size, kernel_size_2;
+
+  anchor = Point( -1 , -1 );
+  delta = 0;
+  ddepth = -1;
+
+  for(int i = 0 ; i < 5 ; i++)
+  {
+    int ind = 0;
+    for(int i=0;i<5;i++)
+    {
+      kernel_size = 3 + 2*( ind%5 );
+      kernel = Mat::ones( kernel_size, kernel_size, CV_32F )/ (float)(kernel_size*kernel_size);
+      filter2D(imagen, sinRuido, ddepth , kernel, anchor, delta, BORDER_DEFAULT );
+    }
+  }
+  guardarIMagen(sinRuido,"sin_Ruido");
+  return sinRuido;
+}
+
+Mat prepararImagen(Mat imagen)
+{
+  Mat resultado;
+  resultado = eliminarRuido(imagen);
+  return imagen;
+}
+
 void llenarLista()
 {
   if(imagenes.empty())
@@ -42,7 +82,10 @@ int main( int argc, char** argv )
     return( -1);
   }
 
+  //-------- INICIO DEL PROGRAMA ---------
+
   llenarLista();
+  Mat imagenF = prepararImagen(image);
 
   if(!imagenes.empty())
   {
