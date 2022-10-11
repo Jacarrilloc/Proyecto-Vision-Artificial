633fd0940d0a26d3765b8d09ff8243b73df68f25
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-10-11 17:32:13 -0500

    cambios en el proyecto

--- proyecto.cxx
+++ proyecto.cxx
@@ -32,20 +32,47 @@ void guardarIMagen(Mat imagen,String nombre)
   cout<<"-----IMAGEN " << nombre << ".png CREADA----"<<endl; 
 }
 
+Mat umbralizar (Mat imagen)
+{
+  Mat imagenGris, resultado;
+  int threshold_value = 1;
+  int threshold_type = 3;
+  int  max_value = 255;
+  int  max_type = 4;
+  int  max_BINARY_value = 255;
+  cvtColor( imagen, imagenGris, cv::COLOR_BGR2GRAY );
+  double value = cv::threshold(imagenGris, resultado, 0, 255, THRESH_OTSU);
+  threshold( imagenGris, resultado, value, max_BINARY_value,threshold_type );
+  guardarIMagen(resultado,"umbralizado");
+  return resultado;
+}
+
+Mat Erosion(Mat imagen)
+{
+  Mat resultado;
+  int erosion_elem = 0;
+  int erosion_size = 0;
+  int dilation_elem = 0;
+  int dilation_size = 0;
+  int const max_elem = 2;
+  int const max_kernel_size = 21;
+
+  int erosion_type = 0;
+  if( erosion_elem == 0 ){ erosion_type = MORPH_RECT; }
+  else if( erosion_elem == 1 ){ erosion_type = MORPH_CROSS; }
+  else if( erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }
+  Mat element = getStructuringElement( erosion_type,Size( 2*erosion_size + 1, 2*erosion_size+1 ),Point( erosion_size, erosion_size ) );
+  erode( imagen, resultado, element );
+  guardarIMagen(resultado,"Erosion");
+  return resultado;
+}
+
 Mat canalesRGB(Mat image)
 {
-  // Crear canales de las Imagenes
-
-  // Canal Rojo
   Mat rImg = Mat::zeros( image.size( ), CV_8UC3 );
-  
-  // Canal Verde
   Mat gImg = Mat::zeros( image.size( ), CV_8UC3 );
-
-  // Canal Azul
   Mat bImg = Mat::zeros( image.size( ), CV_8UC3 );
-   
-  // Fill color channel images
+
   MatIterator_< Vec3b > it, crIt, cgIt, cbIt, rgbIt, end, endr, endg, endb, endrgb;
   it = image.begin< Vec3b >( );
   crIt = rImg.begin< Vec3b >( );
@@ -68,34 +95,23 @@ Mat canalesRGB(Mat image)
   	(*cbIt)[0] = (*it)[0];
   	(*cbIt)[1] = 0;
   	(*cbIt)[2] = 0;
-
-  } // rof
-
-  // Escribir Resultados de Canales
-
+  }
+  /*
   guardarIMagen(rImg,"canal_Rojo");
   guardarIMagen(gImg,"canal_Verde");
-  guardarIMagen(bImg,"canal_Azul");
-
-  //Compresion de Tamaño
+  guardarIMagen(bImg,"canal_Azul");*/
 
   Mat res_img_R = Mat::zeros( rImg.size( ), CV_8UC3 );
   Mat res_img_B = Mat::zeros( gImg.size( ), CV_8UC3 );
   Mat res_img_G  = Mat::zeros( bImg.size( ), CV_8UC3 );
 
-
   resize(rImg, res_img_R, Size(), 0.75 , 0.75 , INTER_LINEAR);  
   resize(gImg, res_img_G, Size(), 0.5 , 0.5 , INTER_LINEAR);
   resize(bImg, res_img_B, Size(), 0.25 , 0.25 , INTER_LINEAR);
 
-  //Escribir Resultados de Compresion
-
-
-  guardarIMagen(res_img_R,"canal_Rojo_comprimido");
+  /*guardarIMagen(res_img_R,"canal_Rojo_comprimido");
   guardarIMagen(res_img_G,"canal_Verde_comprimido");
-  guardarIMagen(res_img_B,"canal_Azul_comprimido");
-
-  //Inicio de Reescalado
+  guardarIMagen(res_img_B,"canal_Azul_comprimido");*/
 
   Mat res_img_R_2 = Mat::zeros( res_img_R.size( ), CV_8UC3 );
   Mat res_img_G_2  = Mat::zeros( res_img_B.size( ), CV_8UC3 );
@@ -105,15 +121,9 @@ Mat canalesRGB(Mat image)
   resize(res_img_G, res_img_G_2, Size(), 2 , 2 , INTER_LINEAR);  
   resize(res_img_B, res_img_B_2, Size(), 4 , 4 , INTER_LINEAR);  
 
-  //Escribir Resultados
-
-  guardarIMagen(res_img_R_2,"canal_Rojo_reescalado");
+  /*guardarIMagen(res_img_R_2,"canal_Rojo_reescalado");
   guardarIMagen(res_img_R_2,"canal_Verde_reescalado");
-  guardarIMagen(res_img_B_2,"canal_Azul_reescalado");
-
-  //Reconstruir Imagen
-
-  // From color channel images, reconstruct the original color image
+  guardarIMagen(res_img_B_2,"canal_Azul_reescalado");*/
 
   Mat rgbImg = Mat::zeros( image.size( ), CV_8UC3 );
 
@@ -133,12 +143,7 @@ Mat canalesRGB(Mat image)
   	(*rgbIt_2)[1] = (*cgIt_2)[1];
   	(*rgbIt_2)[2] = (*crIt_2)[2];
   }
-
-  //Escribir Resultados
-
   guardarIMagen(rgbImg,"canal_rRGB");
-
-  //Comparacion de Imagenes reescaladas y Original
 
   Mat resultado = Mat::zeros( image.size( ), CV_8UC3 );
 
@@ -156,16 +161,11 @@ Mat canalesRGB(Mat image)
   for( ; ItDeferenciaI != ItDeferenciaF, ItOriginalI != ItOriginalF, ItResultadoI != ItResultadoF ; ItDeferenciaI++, ItOriginalI++, ItResultadoI++){
     (*ItDeferenciaI) = (*ItResultadoI) - (*ItOriginalI);
   }
-
-  //Escribir Resultado
-
   guardarIMagen(resultado,"canal_Diferencia");
-
   return resultado;
-
 }
 
-/*Mat metodoDistorsion(Mat srcFrame, int k1) 
+Mat metodoDistorsion(Mat srcFrame, int k1) 
 {    
   Mat map_x, map_y, output;
   double Cy = (double)srcFrame.cols/2;
@@ -180,27 +180,9 @@ Mat canalesRGB(Mat image)
         map_y.at<float>(x,y) = (double) ((x-Cx)/(1 + double(k1/1000000.0)*r2)+Cx); // la posicion relativa del punto al centro
       }
   }
-  remap(srcFrame, output, map_x, map_y, CV_INTER_LINEAR);
+  remap(srcFrame, output, map_x, map_y, INTER_LINEAR);
+  guardarIMagen(output,"Distorsion");
   return output;
-}*/
-
-Mat segmentarImg(Mat imagen)
-{
-  int threshold_value = 4;
-    // Valid Values: 0, 1, 2, 3, 4
-  int threshold_type = 1; 
-    // maxVal, useful for threshold_type 1 and 2
-  int maxVal = 255;
-    // Source image
-  Mat src = imagen;
-  Mat src_gray, dst;
-    // Convert the image to GrayScale
-  cvtColor(src, src_gray, cv::COLOR_BGR2GRAY);
-  guardarIMagen(src_gray,"escala_gris");
-  
-  threshold(src_gray, dst, threshold_value,maxVal, threshold_type);
-  guardarIMagen(dst,"segmentado");
-  return dst;
 }
 
 Mat eliminarRuido(Mat imagen)
@@ -215,9 +197,12 @@ Mat eliminarRuido(Mat imagen)
 Mat prepararImagen(Mat imagen)
 {
   Mat resultado;
-  Mat prueba = segmentarImg(imagen);
-  resultado = canalesRGB(imagen);
-  resultado = eliminarRuido(resultado); 
+
+  resultado = umbralizar(imagen); 
+  resultado = canalesRGB(resultado);
+  Mat prueba = metodoDistorsion(imagen,-1);
+  resultado = eliminarRuido(resultado);
+  //resultado = umbralizar(resultado);
   return resultado;
 }
 
