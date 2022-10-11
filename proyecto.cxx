66c39f1f95fe0a6fd66a93e2e9248848e9ee4599
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-10-10 20:46:17 -0500

    Carpeta de Billetes

--- proyecto.cxx
+++ proyecto.cxx
@@ -24,6 +24,25 @@ void guardarIMagen(Mat imagen,String nombre)
   cout<<"-----IMAGEN " << nombre << ".png CREADA----"<<endl; 
 }
 
+Mat metodoDistorsion(Mat srcFrame, int k1) 
+{    
+  Mat map_x, map_y, output;
+  double Cy = (double)srcFrame.cols/2;
+  double Cx = (double)srcFrame.rows/2;
+  map_x.create(srcFrame.size(), CV_32FC1);
+  map_y.create(srcFrame.size(), CV_32FC1);
+    
+  for (int x=0; x<map_x.rows; x++) {
+    for (int y=0; y<map_y.cols; y++) {
+      double r2 = (x-Cx)*(x-Cx) + (y-Cy)*(y-Cy);
+        map_x.at<float>(x,y) = (double) ((y-Cy)/(1 + double(k1/1000000.0)*r2)+Cy); // se suma para obtener la posicion absoluta
+        map_y.at<float>(x,y) = (double) ((x-Cx)/(1 + double(k1/1000000.0)*r2)+Cx); // la posicion relativa del punto al centro
+      }
+  }
+  remap(srcFrame, output, map_x, map_y, CV_INTER_LINEAR);
+  return output;
+}
+
 Mat eliminarRuido(Mat imagen)
 {
   Mat sinRuido;
