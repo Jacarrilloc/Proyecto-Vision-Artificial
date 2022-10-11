01b3f3a02b6141efc2422df8b35fb85c833509f4
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-10-10 21:07:29 -0500

    banco de imagenes agregados

--- proyecto.cxx
+++ proyecto.cxx
@@ -15,7 +15,6 @@ struct imagenes
   int valor;
 };
 
-
 list<imagenes> imagenesCargadas;
 
 void guardarIMagen(Mat imagen,String nombre)
@@ -24,7 +23,140 @@ void guardarIMagen(Mat imagen,String nombre)
   cout<<"-----IMAGEN " << nombre << ".png CREADA----"<<endl; 
 }
 
-Mat metodoDistorsion(Mat srcFrame, int k1) 
+Mat canalesRGB(Mat image)
+{
+  // Crear canales de las Imagenes
+
+  // Canal Rojo
+  Mat rImg = Mat::zeros( image.size( ), CV_8UC3 );
+  
+  // Canal Verde
+  Mat gImg = Mat::zeros( image.size( ), CV_8UC3 );
+
+  // Canal Azul
+  Mat bImg = Mat::zeros( image.size( ), CV_8UC3 );
+   
+  // Fill color channel images
+  MatIterator_< Vec3b > it, crIt, cgIt, cbIt, rgbIt, end, endr, endg, endb, endrgb;
+  it = image.begin< Vec3b >( );
+  crIt = rImg.begin< Vec3b >( );
+  cgIt = gImg.begin< Vec3b >( );
+  cbIt = bImg.begin< Vec3b >( );
+  end = image.end< Vec3b >( );
+  endr = rImg.end< Vec3b >( );
+  endg = gImg.end< Vec3b >( );
+  endb = bImg.end< Vec3b >( );
+  for(  ; it != end, crIt != endr, cgIt != endg, cbIt != endb; ++it, ++crIt, ++cgIt, ++cbIt)
+  {
+  	(*crIt)[0] = 0;
+  	(*crIt)[1] = 0;
+  	(*crIt)[2] = (*it)[2];
+
+  	(*cgIt)[0] = 0;
+  	(*cgIt)[1] = (*it)[1];
+  	(*cgIt)[2] = 0;
+  	
+  	(*cbIt)[0] = (*it)[0];
+  	(*cbIt)[1] = 0;
+  	(*cbIt)[2] = 0;
+
+  } // rof
+
+  // Escribir Resultados de Canales
+
+  guardarIMagen(rImg,"canal_Rojo");
+  guardarIMagen(gImg,"canal_Verde");
+  guardarIMagen(bImg,"canal_Azul");
+
+  //Compresion de Tamaño
+
+  Mat res_img_R = Mat::zeros( rImg.size( ), CV_8UC3 );
+  Mat res_img_B = Mat::zeros( gImg.size( ), CV_8UC3 );
+  Mat res_img_G  = Mat::zeros( bImg.size( ), CV_8UC3 );
+
+
+  resize(rImg, res_img_R, Size(), 0.75 , 0.75 , INTER_LINEAR);  
+  resize(gImg, res_img_G, Size(), 0.5 , 0.5 , INTER_LINEAR);
+  resize(bImg, res_img_B, Size(), 0.25 , 0.25 , INTER_LINEAR);
+
+  //Escribir Resultados de Compresion
+
+
+  guardarIMagen(res_img_R,"canal_Rojo_comprimido");
+  guardarIMagen(res_img_R,"canal_Verde_comprimido");
+  guardarIMagen(res_img_B,"canal_Azul_comprimido");
+
+  //Inicio de Reescalado
+
+  Mat res_img_R_2 = Mat::zeros( res_img_R.size( ), CV_8UC3 );
+  Mat res_img_G_2  = Mat::zeros( res_img_B.size( ), CV_8UC3 );
+  Mat res_img_B_2 = Mat::zeros( res_img_G.size( ), CV_8UC3 );
+
+  resize(res_img_R, res_img_R_2, Size(), 1.333 , 1.333 , INTER_LINEAR);
+  resize(res_img_G, res_img_G_2, Size(), 2 , 2 , INTER_LINEAR);  
+  resize(res_img_B, res_img_B_2, Size(), 4 , 4 , INTER_LINEAR);  
+
+  //Escribir Resultados
+
+  guardarIMagen(res_img_R_2,"canal_Rojo_reescalado");
+  guardarIMagen(res_img_R_2,"canal_Verde_reescalado");
+  guardarIMagen(res_img_B_2,"canal_Azul_reescalado");
+
+  //Reconstruir Imagen
+
+  // From color channel images, reconstruct the original color image
+
+  Mat rgbImg = Mat::zeros( image.size( ), CV_8UC3 );
+
+  MatIterator_< Vec3b > rgbIt_2,crIt_2,cgIt_2,cbIt_2,endr_2,endg_2,endb_2,endrgb_2;
+
+  crIt_2 = res_img_R_2.begin< Vec3b >( );
+  cgIt_2 = res_img_G_2.begin< Vec3b >( );
+  cbIt_2 = bImg.begin< Vec3b >( );
+  rgbIt_2 = rgbImg.begin< Vec3b >( );
+  endr_2 = res_img_R_2.end< Vec3b >( );
+  endg_2 = res_img_G_2.end< Vec3b >( );
+  endb_2 = res_img_B_2.end< Vec3b >( );
+  endrgb_2 = rgbImg.end< Vec3b >( );
+  for(  ; crIt_2 != endr_2, cgIt_2 != endg_2, cbIt_2 != endb_2, rgbIt_2 != endrgb_2; ++crIt_2, ++cgIt_2, ++cbIt_2, ++rgbIt_2)
+  {
+  	(*rgbIt_2)[0] = (*cbIt_2)[0];
+  	(*rgbIt_2)[1] = (*cgIt_2)[1];
+  	(*rgbIt_2)[2] = (*crIt_2)[2];
+  }
+
+  //Escribir Resultados
+
+  guardarIMagen(rgbImg,"canal_rRGB");
+
+  //Comparacion de Imagenes reescaladas y Original
+
+  Mat resultado = Mat::zeros( image.size( ), CV_8UC3 );
+
+  MatIterator_< Vec3b > ItOriginalI, ItOriginalF, ItResultadoI , ItResultadoF, ItDeferenciaI, ItDeferenciaF;
+
+  ItOriginalI = image.begin< Vec3b >( );
+  ItOriginalF = image.end< Vec3b >( );
+
+  ItResultadoI = rgbImg.begin< Vec3b >( );
+  ItResultadoF = rgbImg.end< Vec3b >( );
+
+  ItDeferenciaI = resultado.begin< Vec3b >( );
+  ItDeferenciaF = resultado.end< Vec3b >( );
+
+  for( ; ItDeferenciaI != ItDeferenciaF, ItOriginalI != ItOriginalF, ItResultadoI != ItResultadoF ; ItDeferenciaI++, ItOriginalI++, ItResultadoI++){
+    (*ItDeferenciaI) = (*ItResultadoI) - (*ItOriginalI);
+  }
+
+  //Escribir Resultado
+
+  guardarIMagen(resultado,"canal_Diferencia");
+
+  return resultado;
+
+}
+
+/*Mat metodoDistorsion(Mat srcFrame, int k1) 
 {    
   Mat map_x, map_y, output;
   double Cy = (double)srcFrame.cols/2;
@@ -41,7 +173,7 @@ Mat metodoDistorsion(Mat srcFrame, int k1)
   }
   remap(srcFrame, output, map_x, map_y, CV_INTER_LINEAR);
   return output;
-}
+}*/
 
 Mat eliminarRuido(Mat imagen)
 {
@@ -56,7 +188,8 @@ Mat prepararImagen(Mat imagen)
 {
   Mat resultado;
   resultado = eliminarRuido(imagen);
-  return imagen;
+  resultado = canalesRGB(resultado);
+  return resultado;
 }
 
 void llenarLista()
